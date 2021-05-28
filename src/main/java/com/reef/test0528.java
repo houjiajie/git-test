c46d30445c61de071d83b4a96f221fd23651c867
houjiajie <1257051547@qq.com>
2021-05-28 21:56:31 +0800

    third commoit

--- src/main/java/com/reef/test0528.java
+++ src/main/java/com/reef/test0528.java
@@ -4,5 +4,6 @@ public class test0528 {
     public static void main(String[] args) {
         System.out.println("88888");
         System.out.println("66666");
+        System.out.println("9999");
     }
 }
